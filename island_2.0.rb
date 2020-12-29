e699bea526bf49f876f0357fe0519ae249ea4d2d
akkun016 <nakataakito@icloud.com>
2020-12-29 21:21:14 +0900

    islandの練習結果

---
+++ island_2.0.rb
@@ -0,0 +1,52 @@
+def island(array)
+    @open = []
+    count = 0
+    for i in 0..(@h - 1) do
+        for j in 0..(@w - 1) do
+            if array[i][j] == 1
+                @open << [i, j]
+                @closed = []
+                until @open.empty?
+                    item = @open.shift
+                    if array[item[0]][item[1]] == 0
+                        next
+                    end
+                    @closed << item
+                    array[item[0]][item[1]] = 0
+
+                    if_func(array, item[0] + 1, item[1])
+                    if_func(array, item[0] - 1, item[1])
+                    if_func(array, item[0], item[1] + 1)
+                    if_func(array, item[0], item[1] - 1)
+                end
+                count += 1
+            end
+        end
+    end
+    return count
+end
+
+def if_func(array, h, w)
+    if h != @h && h != -1 && w != @w && w != -1
+        if array[h][w] == 1
+            @open << [h, w]
+        end
+    end
+end
+
+nums = gets.chomp.split(" ").map(&:to_i)
+@w, @h = nums[0], nums[1]
+grahp = []
+
+@h.times do
+    grahp << gets.chomp.split(" ").map(&:to_i)
+end
+p island(grahp)
+
+# ------------------------------------------------------------------
+# 改良内容
+# チェック済みを配列に入れて include? で判別するのではなく、
+# 盤面自体の数値を0に変え、チェックしている部分が0であれば次に飛ぶように変更
+
+# include? >>> if での判定
+# ------------------------------------------------------------------
